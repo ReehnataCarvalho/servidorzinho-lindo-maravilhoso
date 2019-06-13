3f7a00d0fd06d88024438c74f2207e3a1d4ec002
ReehnataCarvalho <reehnataoliveira592@gmail.com>
2019-06-12 22:25:35 -0300

    adicionamos um formulario para adicionar imagens a API, adicionei imagens.

--- ComidasSchema.js
+++ ComidasSchema.js
@@ -5,7 +5,8 @@ const ComidasSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   nome: { type: String, required: true },
   descricao: { type: String },
-  valor: { type: Number } // opcional
+  valor: { type: Number }, // opcional
+  imagem: {type: String }
 })
 
 // é a nossa coleção de comidas
